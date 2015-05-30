dab4eea9598f91a56a8f1faf03da039917045ac4
monochromegane <dev.kuro.obi@gmail.com>
2015-05-30 11:25:23 +0900

    Added comments for a package and exported functions.

--- conflag.go
+++ conflag.go
@@ -1,7 +1,9 @@
+// Package conflag provides configuration parser for command-line flag.
 package conflag
 
 import "os"
 
+// ArgsFrom make arguments for command-line flag from configuration file.
 func ArgsFrom(conf string, positions ...string) ([]string, error) {
 	if _, err := os.Stat(conf); err != nil {
 		return nil, err
