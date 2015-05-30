4e0e3b291f912108b26c90bffc59227e40637310
monochromegane <dev.kuro.obi@gmail.com>
2015-05-30 11:51:30 +0900

    Removed development codes from main.

--- cmd/conflag/main.go
+++ cmd/conflag/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/monochromegane/conflag"
 )
 
 func main() {
-	args, err := conflag.ArgsFrom("test.json", "args")
-	fmt.Printf("%v, %v\n", args, err)
+	args, err := conflag.ArgsFrom(os.Args[1], os.Args[2:]...)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+	fmt.Printf("%v\n", args)
 }
